731ab35297deacb04daac372eb78bad7843a4e6b
Julio Amado <33491254+BeFlow@users.noreply.github.com>
2018-10-16 14:27:21 -0300

    else syntax linha 450 tentativa 3

--- __init__.py
+++ __init__.py
@@ -446,9 +446,9 @@ def newMenuItem(restaurant_id):
 
     session.commit()
     flash('New Menu %s Item Successfully Created' % (newItem.name))
-    	return redirect(url_for('showMenu', restaurant_id=restaurant_id))
-    else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+    return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+else:
+	return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit',

f742ad303407bc20ea8a6becd79e04dd00b159db
Julio Amado <33491254+BeFlow@users.noreply.github.com>
2018-10-16 15:20:54 -0300

    database line 1

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
- import Column, ForeignKey, Integer, String
+import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine

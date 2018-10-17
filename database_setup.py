3f4e5f1e0c4eb5c8f803e63deba84af6d2763507
Julio Amado <33491254+BeFlow@users.noreply.github.com>
2018-10-17 14:55:15 -0300

    troca de database linha 123

--- database_setup.py
+++ database_setup.py
@@ -120,7 +120,7 @@ class MenuItem(Base):
         }
 
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('postgresql://catalog:grader@localhost/catalog')
 
 
 Base.metadata.create_all(engine)
